536bd1fbae2482dcddd9046570118bd7a8d62d95
marialapc <marialopez.apc@gmail.com>
2023-03-23 11:56:06 +0100

    cursor pointer

---
+++ main.js
@@ -0,0 +1,9 @@
+// var pointer = document.getElementById("pointer");
+
+// document.addEventListener("mousemove", function(event) {
+//   var x = event.clientX;
+//   var y = event.clientY;
+  
+//   pointer.style.left = x + "px";
+//   pointer.style.top = y + "px";
+// });
