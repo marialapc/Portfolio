561086ca8ffa76090bd40f3fa44e51031fbb3013
marialapc <marialopez.apc@gmail.com>
2023-03-24 10:07:30 +0100

    animation text

--- main.js
+++
@@ -1,9 +0,0 @@
-// var pointer = document.getElementById("pointer");
-
-// document.addEventListener("mousemove", function(event) {
-//   var x = event.clientX;
-//   var y = event.clientY;
-  
-//   pointer.style.left = x + "px";
-//   pointer.style.top = y + "px";
-// });
