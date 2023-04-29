9bc5ad3fba91989ec689038717cf85f4ea30ae8c
marialapc <marialopez.apc@gmail.com>
2023-04-29 12:20:13 +0200

    try shadows in mobile

--- bb8.js
+++ bb8.js
@@ -4,7 +4,7 @@ const droidBody = document.querySelector(".droid-body");
 const droidEyes = document.querySelector(".eyes");
 
 let timeoutId = null;
-let currentRotation = '';
+let currentRotation = "";
 
 function moveDroid(event) {
   const mouseX = event.clientX;
@@ -13,24 +13,23 @@ function moveDroid(event) {
   if (distance > 0) {
     droid.style.left = droid.offsetLeft + 5 + "px";
     droidBody.style.transform = "scaleX(-1)";
-    droidBody.classList.add('rotate-right');
-    droidBody.classList.remove('rotate-left');
-    droidEyes.style.transform = "scaleX(-1)"
-    currentRotation = 'rotate-right';
-    
+    droidBody.classList.add("rotate-right");
+    droidBody.classList.remove("rotate-left");
+    droidEyes.style.transform = "scaleX(-1)";
+    currentRotation = "rotate-right";
   } else if (distance < 0) {
     droid.style.left = droid.offsetLeft - 5 + "px";
     droidBody.style.transform = "scaleX(1)";
     droidEyes.style.transform = "scaleX(1)";
-    droidBody.classList.remove('rotate-right');
-    droidBody.classList.add('rotate-left');
-    currentRotation = 'rotate-left';
+    droidBody.classList.remove("rotate-right");
+    droidBody.classList.add("rotate-left");
+    currentRotation = "rotate-left";
   }
-  
+
   clearTimeout(timeoutId);
   timeoutId = setTimeout(() => {
-    droidBody.classList.remove('rotate-right');
-    droidBody.classList.remove('rotate-left');
+    droidBody.classList.remove("rotate-right");
+    droidBody.classList.remove("rotate-left");
     droidBody.classList.add(currentRotation);
   }, 100);
 }
