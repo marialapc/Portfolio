5866c384d27ecc1276417334227aa8d0cf1af8e5
marialapc <marialopez.apc@gmail.com>
2023-04-29 11:36:25 +0200

    move to the right direction

--- bb8.js
+++ bb8.js
@@ -1,6 +1,7 @@
 const droid = document.querySelector(".droid");
 const body = document.body;
-const droidBody = document.querySelector(".droid-body")
+const droidBody = document.querySelector(".droid-body");
+const droidEyes = document.querySelector(".eyes");
 
 function moveDroid(event) {
   const mouseX = event.clientX;
@@ -8,12 +9,18 @@ function moveDroid(event) {
 
   if (distance > 0) {
     droid.style.left = droid.offsetLeft + 5 + "px";
-    droid.style.transform = "scaleX(1)";
-    droidBody.classList.add('rotate');
+    droidBody.style.transform = "scaleX(-1)";
+    droidBody.classList.add('rotate-right');
+    droidBody.classList.remove('rotate-left');
+    droidEyes.style.transform = "scaleX(-1)"
     
   } else if (distance < 0) {
     droid.style.left = droid.offsetLeft - 5 + "px";
-    droid.style.transform = "scaleX(-1)";
+    droidBody.style.transform = "scaleX(1)";
+    droidEyes.style.transform = "scaleX(1)";
+    droidBody.classList.remove('rotate-right');
+    droidBody.classList.add('rotate-left');
+    
   }
 }
 
