bdbff4f78647ff1313520faa0b40edbc7342f767
marialapc <marialopez.apc@gmail.com>
2023-04-29 12:06:16 +0200

    rotate infinite

--- bb8.js
+++ bb8.js
@@ -3,6 +3,9 @@ const body = document.body;
 const droidBody = document.querySelector(".droid-body");
 const droidEyes = document.querySelector(".eyes");
 
+let timeoutId = null;
+let currentRotation = '';
+
 function moveDroid(event) {
   const mouseX = event.clientX;
   const distance = mouseX - droid.offsetLeft;
@@ -13,6 +16,7 @@ function moveDroid(event) {
     droidBody.classList.add('rotate-right');
     droidBody.classList.remove('rotate-left');
     droidEyes.style.transform = "scaleX(-1)"
+    currentRotation = 'rotate-right';
     
   } else if (distance < 0) {
     droid.style.left = droid.offsetLeft - 5 + "px";
@@ -20,8 +24,15 @@ function moveDroid(event) {
     droidEyes.style.transform = "scaleX(1)";
     droidBody.classList.remove('rotate-right');
     droidBody.classList.add('rotate-left');
-    
+    currentRotation = 'rotate-left';
   }
+  
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    droidBody.classList.remove('rotate-right');
+    droidBody.classList.remove('rotate-left');
+    droidBody.classList.add(currentRotation);
+  }, 100);
 }
 
 body.addEventListener("mousemove", moveDroid);
